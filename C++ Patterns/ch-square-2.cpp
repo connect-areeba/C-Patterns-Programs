2b026f84cd33d244f747c4aa1764c5db7200726d
Areeba <connect.areeba@gmail.com>
2025-12-30 01:11:07 +0500

    Add files via upload

--- C++ Patterns/ch-square-2.cpp
+++ C++ Patterns/ch-square-2.cpp
@@ -4,10 +4,12 @@ int main(){
 	int n;
 	cout<<"enter n: ";
 	cin>>n;
+	char ch = 'A';
 	
-    for(int i=0; i<n; i++){
-    	for(int j=0; j<i+1; j++){
-    		cout<<"* ";
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cout<<ch<<" ";
+			ch++;
 		}
 		cout<<"\n";
 	}
